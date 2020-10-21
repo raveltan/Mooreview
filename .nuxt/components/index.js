6c2e6c2ea29e74ddc2894c5bccf0de073164260b
Ravel Tan <raveltanjaya@outlook.com>
2020-10-21 21:46:21 +0700

    added disT

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Login } from '../../components/login.vue'
-
-export const LazyLogin = import('../../components/login.vue' /* webpackChunkName: "components/login" */).then(c => c.default || c)
